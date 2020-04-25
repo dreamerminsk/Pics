8b8fe49f96feee045e4acb72a0e046142b9d81d5
dreamerminsk <tutmirage@gmail.com>
2020-04-25 14:38:15 +0300

    VInt

--- Pics/Readers/MkvFile.cs
+++ Pics/Readers/MkvFile.cs
@@ -7,6 +7,35 @@ namespace Pics.Readers
         public static long EBMLID = 0x1A45DFA3;
     }
 
+    public class VInt
+    {
+        private long value;
+
+        public VInt(long value)
+        {
+            this.Value = value;
+        }
+
+        public long Value { get => value; set => this.value = value; }
+
+        public void WriteTo(Stream stream)
+        {
+
+        }
+
+        public static VInt ReadFrom(Stream stream)
+        {
+            var firstByte = stream.ReadByte();
+            if (firstByte > 127)
+            {
+                return new VInt((long)firstByte & 0x1FL);
+            }
+
+            return new VInt(0);
+        }
+
+    }
+
     public class EBMLElement
     {
 
