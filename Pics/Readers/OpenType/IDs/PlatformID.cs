4222ea0c3d95d5d26444c996befa463429cf8fd7
dreamerminsk <tutmirage@gmail.com>
2020-05-02 09:49:18 +0300

    Formatting classes

--- Pics/Readers/OpenType/IDs/PlatformID.cs
+++ Pics/Readers/OpenType/IDs/PlatformID.cs
@@ -1,9 +1,11 @@
-namespace Pics.Readers.OpenType.IDs {
-	public enum PlatformID : ushort {
-		Unicode = 0,
-		Macintosh = 1,
-		ISO = 2,
-		Windows = 3,
-		Custom = 4
-	}
+namespace Pics.Readers.OpenType.IDs
+{
+    public enum PlatformID : ushort
+    {
+        Unicode = 0,
+        Macintosh = 1,
+        ISO = 2,
+        Windows = 3,
+        Custom = 4
+    }
 }
