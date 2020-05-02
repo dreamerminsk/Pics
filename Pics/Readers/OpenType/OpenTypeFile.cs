cb4e974d96f4511540e497cc94594116c626ee5f
dreamerminsk <tutmirage@gmail.com>
2020-05-02 09:43:54 +0300

    TableRecord formatting

--- Pics/Readers/OpenType/OpenTypeFile.cs
+++ Pics/Readers/OpenType/OpenTypeFile.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Pics.Readers.OpenType
+﻿namespace Pics.Readers.OpenType
 {
     public class OpenTypeFile
     {
--- Pics/Readers/OpenType/TableRecord.cs
+++ Pics/Readers/OpenType/TableRecord.cs
@@ -1,41 +1,48 @@
 using System.Text;
 
-namespace Pics.Readers.OpenType {
-	public class TableRecord {
-		public string tableTag;
+namespace Pics.Readers.OpenType
+{
+    public class TableRecord
+    {
+        public string tableTag;
 
-		public uint checkSum;
+        public uint checkSum;
 
-		public uint offset;
+        public uint offset;
 
-		public uint length;
+        public uint length;
 
-		public static TableRecord[] ReadArray(BinaryReaderFont reader, int count) {
-			TableRecord[] array = new TableRecord[count];
-			for (int i = 0; i < count; i++) {
-				array[i] = Read(reader);
-			}
-			return array;
-		}
+        public static TableRecord[] ReadArray(BinaryReaderFont reader, int count)
+        {
+            TableRecord[] array = new TableRecord[count];
+            for (int i = 0; i < count; i++)
+            {
+                array[i] = Read(reader);
+            }
+            return array;
+        }
 
-		public static TableRecord Read(BinaryReaderFont reader) {
-			return new TableRecord {
-				tableTag = reader.ReadTag(),
-				checkSum = reader.ReadUInt32(),
-				offset = reader.ReadUInt32(),
-				length = reader.ReadUInt32()
-			};
-		}
+        public static TableRecord Read(BinaryReaderFont reader)
+        {
+            return new TableRecord
+            {
+                tableTag = reader.ReadTag(),
+                checkSum = reader.ReadUInt32(),
+                offset = reader.ReadUInt32(),
+                length = reader.ReadUInt32()
+            };
+        }
 
-		public override string ToString() {
-			StringBuilder builder = new StringBuilder();
-			builder.AppendLine("{");
-			builder.AppendFormat("\t\"tableTag\": \"{0}\",\n", tableTag);
-			builder.AppendFormat("\t\"checkSum\": 0x{0:X8},\n", checkSum);
-			builder.AppendFormat("\t\"offset\": 0x{0:X8},\n", offset);
-			builder.AppendFormat("\t\"length\": 0x{0:X8}\n", length);
-			builder.Append("}");
-			return builder.ToString();
-		}
-	}
+        public override string ToString()
+        {
+            StringBuilder builder = new StringBuilder();
+            builder.AppendLine("{");
+            builder.AppendFormat("\t\"tableTag\": \"{0}\",\n", tableTag);
+            builder.AppendFormat("\t\"checkSum\": 0x{0:X8},\n", checkSum);
+            builder.AppendFormat("\t\"offset\": 0x{0:X8},\n", offset);
+            builder.AppendFormat("\t\"length\": 0x{0:X8}\n", length);
+            builder.Append("}");
+            return builder.ToString();
+        }
+    }
 }
