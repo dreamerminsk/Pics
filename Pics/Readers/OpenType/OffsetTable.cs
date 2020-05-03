cec4676478de16bdef95dfd09acdfe591befe16d
dreamerminsk <tutmirage@gmail.com>
2020-05-03 12:39:16 +0300

    OffsetTable

--- Pics/Readers/OpenType/OffsetTable.cs
+++ Pics/Readers/OpenType/OffsetTable.cs
@@ -1,18 +1,52 @@
 ﻿using Pics.View;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Pics.Readers.OpenType
 {
     public class OffsetTable : IItemable
     {
+        private Int64 pos = 0;
+        private UInt32 sfntVersion;
+        private UInt16 numTables;
+        private UInt16 searchRange;
+        private UInt16 entrySelector;
+        private UInt16 rangeShift;
+
+        public OffsetTable()
+        {
+
+        }
+
         public List<ListViewItem> Items()
         {
-            throw new NotImplementedException();
+            var items = new List<ListViewItem>();
+
+            var item = new ListViewItem("sfntVersion");
+            item.SubItems.Add(pos.ToString());
+            item.SubItems.Add(sizeof(UInt32).ToString());
+            items.Add(item);
+
+            item = new ListViewItem("numTables");
+            item.SubItems.Add((pos + sizeof(UInt32)).ToString());
+            item.SubItems.Add(sizeof(UInt16).ToString());
+            items.Add(item);
+            item = new ListViewItem("searchRange");
+            item.SubItems.Add("182");
+            item.SubItems.Add("160");
+            items.Add(item);
+
+            item = new ListViewItem("entrySelector");
+            item.SubItems.Add("182");
+            item.SubItems.Add("160");
+            items.Add(item);
+
+            item = new ListViewItem("rangeShift");
+            item.SubItems.Add("182");
+            item.SubItems.Add("160");
+            items.Add(item);
+            return items;
         }
     }
 }
--- Pics/Readers/OpenType/TableRecord.cs
+++ Pics/Readers/OpenType/TableRecord.cs
@@ -1,6 +1,5 @@
 using System.IO;
 using System.Text;
-using Pics.Readers.OpenType;
 
 namespace Pics.Readers.OpenType
 {
