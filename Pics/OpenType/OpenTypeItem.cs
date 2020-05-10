fcfec42d1f45e1fc1e1e634b393933ec1ca1c4c8
Micah Teeuws /dKCh/ <tutmirage@gmail.com>
2020-05-10 21:40:17 +0300

    Update OpenTypeItem.cs

--- Pics/OpenType/OpenTypeItem.cs
+++ Pics/OpenType/OpenTypeItem.cs
@@ -5,6 +5,12 @@ namespace Pics.OpenType
 
     public interface OpenTypeItem
     {
+        
+        long Position { get; set; }
+        
+        long Size { get; set; }
+        
+        string Title { get; set; }
 
         OpenTypeItem Parent { get; }
 
