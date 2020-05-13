6d5811b95976d4c7876505df1384d5eb155ec78e
dreamerminsk <tutmirage@gmail.com>
2020-05-13 13:52:01 +0300

    FirestoreDb

--- TalonBY/Form1.cs
+++ TalonBY/Form1.cs
@@ -1,4 +1,6 @@
-﻿using HtmlAgilityPack;
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Cloud.Firestore;
+using HtmlAgilityPack;
 using System;
 using System.Linq;
 using System.Windows.Forms;
@@ -16,12 +18,18 @@ namespace TalonBY
         {
             HtmlWeb web = new HtmlWeb();
             var html = await web.LoadFromWebAsync("https://talon.by/policlinics");
+            //var credential = GoogleCredential.FromFile(@"c:\Users\User\FB\karoMed-59cbeddc2d66.json");
+            //FirestoreDb db = FirestoreDb.Create("karomed-534ba");
+            FirestoreDb db = FirestoreDb.Create();
             var n = 0;
-            HtmlNodeCollection nodes = html.DocumentNode.SelectNodes("//div/*/h5/a");
-            nodes.ToList().ForEach(x =>
-            {                
-                richTextBox1.AppendText((++n).ToString() + ". " + x.InnerText + "\r\n");
-                richTextBox1.AppendText("\t" + x.Attributes["href"].Value + "\r\n");
+            HtmlNodeCollection nodes = html.DocumentNode.SelectNodes("//div[@class='row']");
+            nodes.ToList().ForEach(node =>
+            {
+                var clinic = node.SelectSingleNode(".//h5/a");
+                richTextBox1.AppendText((++n).ToString() + ". " + clinic.InnerText + "\r\n");
+                richTextBox1.AppendText("\t" + clinic.Attributes["href"].Value + "\r\n");
+                var addr = node.SelectSingleNode(".//p/span");
+                richTextBox1.AppendText("\t" + addr.InnerText + "\r\n");
             });
         }
     }
