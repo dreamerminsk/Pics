6ca760d05b1a9db95d96bb48c47d25bb4f193e3d
dreamerminsk <tutmirage@gmail.com>
2020-05-15 13:18:42 +0300

    nnmclub.to

---
+++ Rater/Form1.cs
@@ -0,0 +1,38 @@
+﻿using HtmlAgilityPack;
+using MongoDB.Driver;
+using System;
+using System.Data;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Rater
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            HtmlWeb web = new HtmlWeb();
+            var page = await web.LoadFromWebAsync("http://nnmclub.to/");
+            var torrents = page.DocumentNode.SelectNodes(".//table[@class='pline']");
+            torrents.Where(x => IsTorentBlock(x)).ToList().ForEach(x =>
+              {
+                  var title = x.SelectSingleNode(".//td[@class='pcatHead']");
+                  if (title != null)
+                  {
+                      Console.WriteLine(title.InnerText);
+                  }
+
+              });
+        }
+
+        private bool IsTorentBlock(HtmlNode node)
+        {
+            return node.SelectNodes(".//a[starts-with(@href, 'magnet')]") != null;
+        }
+    }
+}
---
+++ Rater/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Rater")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Rater")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("00258208-0a8f-45e0-b485-51cb2d98061c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
